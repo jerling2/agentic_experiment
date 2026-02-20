77decd8073fcf3f67b1563e98327c5925f18afa5
Joseph Erlinger <josepherlinger@gmail.com>
2026-02-19 16:32:12 -0800

    housekeeping: remove scaffolding

--- src/nodes/process_node.py
+++
@@ -1,9 +0,0 @@
-from langgraph.runtime import Runtime
-
-from src import AppContext, SimpleState
-
-def process_node(state: SimpleState, runtime: Runtime[AppContext]):
-    desc = runtime.context.description
-    user_input = state.input
-
-    return {"results": f"context description {desc} and state.input {user_input}"}
